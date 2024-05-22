3fd0234123895e44211033bd0a73cccde0922b58
NAila Alissa <nailaalissa56@gmail.com>
2024-05-22 11:39:14 +0200

    update widget

--- src/comp/Widget.js
+++ src/comp/Widget.js
@@ -1,9 +1,68 @@
+// import React, { useState } from 'react';
+
+// export default function Widget() {
+//   const [companyName, setCompanyName] = useState('lexicon');
+//   const [iframeSrc, setIframeSrc] = useState(
+//     `https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=lexicon`,
+//   );
+//   const [showLink, setShowLink] = useState(false);
+
+//   const handleCompanyChange = (e) => {
+//     setCompanyName(e.target.value);
+//   };
+
+//   const generateIframeLink = () => {
+//     setIframeSrc(
+//       `https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=${companyName}`,
+//     );
+//     setShowLink(true);
+//   };
+
+//   return (
+//     <div style={{ minHeight: '90vh' }}>
+//       <div style={{ textAlign: 'center' }}>
+//         <h1 className="margin">Map Widget</h1>
+//         <div className="widgetLink">
+//           <select className="selectComany" value={companyName} onChange={handleCompanyChange}>
+//             <option value="lexicon">Lexicon</option>
+//             <option value="acme">acme</option>
+//           </select>
+//           <br />
+//           <button className="generatLink" onClick={generateIframeLink} style={{ margin: '20px' }}>
+//             Generate Link
+//           </button>
+//         </div>
+//         {showLink && (
+//           <>
+//             <h4>
+//               Copy the URL link to embed the map widget
+//               <br />
+//               <br />
+//               <p className="widget">
+//                 &lt;iframe src="
+//                 {`https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=${companyName}`}
+//                 " title="Widget" width="100%" height="600"&gt;&lt;/iframe&gt;
+//               </p>
+//             </h4>
+//             <iframe
+//               src={iframeSrc}
+//               title="Widget"
+//               width="100%"
+//               height="600"
+//               style={{ border: 'none' }}
+//             ></iframe>
+//           </>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
 import React, { useState } from 'react';
 
 export default function Widget() {
   const [companyName, setCompanyName] = useState('lexicon');
   const [iframeSrc, setIframeSrc] = useState(
-    `https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=lexicon`,
+    `https://pinmeto-widget.onrender.com/MapContainer?companyName=lexicon`,
   );
   const [showLink, setShowLink] = useState(false);
 
@@ -12,9 +71,7 @@ export default function Widget() {
   };
 
   const generateIframeLink = () => {
-    setIframeSrc(
-      `https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=${companyName}`,
-    );
+    setIframeSrc(`https://pinmeto-widget.onrender.com/MapContainer?companyName=${companyName}`);
     setShowLink(true);
   };
 
@@ -40,8 +97,8 @@ export default function Widget() {
               <br />
               <p className="widget">
                 &lt;iframe src="
-                {`https://pinmeto-frontend-24.onrender.com/MapContainer?companyName=${companyName}`}
-                " title="Widget" width="100%" height="600"&gt;&lt;/iframe&gt;
+                {`https://pinmeto-widget.onrender.com/MapContainer?companyName=${companyName}`}"
+                title="Widget" width="100%" height="600"&gt;&lt;/iframe&gt;
               </p>
             </h4>
             <iframe
