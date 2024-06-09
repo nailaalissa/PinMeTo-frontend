ecbdbf3c01de75dd9b47fe3ac477e482c04d5486
NAila Alissa <nailaalissa56@gmail.com>
2024-06-09 13:49:17 +0200

    last update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import reportWebVitals from './reportWebVitals';
 
+import MapContainer from './pages/MapContainer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Map from './pages/Map';
+import Widget from './pages/Widget';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <h1>SORROY.........</h1>,
+  },
+  {
+    path: '/Home',
+    element: <Home />,
+  },
+
+  {
+    path: '/Map',
+    element: <Map />,
+  },
+  {
+    path: '/widget',
+    element: <Widget />,
+  },
+  {
+    path: '/MapContainer',
+    element: <MapContainer />,
+  },
+]);
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
-
+reportWebVitals();
 export default App;
